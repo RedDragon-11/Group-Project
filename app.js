c814d8dfc7844677a3834012f6755270f4c61220
RedDragon-11 <mgaddis97@gmail.com>
2024-04-07 10:59:24 -0400

    Removed Comment
    Removed Bad Comment

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const path = require("path");
-const session = require('express-session'); // Import express-session module
+const session = require('express-session'); 
 const app = express();
 
 // Middleware
