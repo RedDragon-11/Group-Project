23661f4300783c0dac941ba11d3d2f7c4ab9c405
RedDragon-11 <mgaddis97@gmail.com>
2024-04-10 19:55:43 -0400

    Search: Fixed Var not Defined
    I made it where the server wouldn't crash upon load in of the page but now there are some... other issues. Doing anything doesn't seem to actually pull a query or anything. Needs a lot more work

--- routes/search.js
+++ routes/search.js
@@ -5,23 +5,51 @@ const router = express.Router();
 
 // This route handles the GET request for displaying the search page
 router.get('/', (req, res) => {
-    res.render('search', { loggedInUser: req.session.user }); // Assuming you have a search.ejs file in your views directory
+    res.render('search', { loggedInUser: req.session.user, searchResults: req.session.results }); // Assuming you have a search.ejs file in your views directory
 });
 
 
 
 // This route handles the POST request for processing the search query
-router.get('/results', (req, res) => {
+ router.get('/', async (req, res) => {
     const searchQuery = req.body.search;
+    if (searchQuery = null){
+        req.session.results = req.session.user
+    }
+    else {
+    try {
+    const results = await WishlistItems.find({createdBy: searchQuery })
+    if (!results) {
+        return('Error querying wishlist items:');
+        return res.status(404).send('No results found');
+    }
+    else {
+        req.session.results = results
+    }
+    
+    res.render('search', { loggedInUser: req.session.user, searchResults: req.session.results });
 
+    } catch (error) {
+        console.error('Server error:', error);
+        res.status(500).send('Internal Server Error');
+    }
+    }
+    }); 
+
+/* router.post('/', (req, res) => {
+    const searchQuery = req.body.search;
+    
     WishlistItems.find({ createdBy: searchQuery })
     .then(items => {
-        res.render('search', { loggedinUser: req.session.user, searchResults: items });
+        res.render('search', { 
+            loggedInUser: req.session.user, 
+            searchResults: items 
+        });
     })
     .catch(error => {
         console.error('Error querying wishlist items:', error);
         res.status(500).send('Internal Server Error');
     });
-});
+}); */
 
 module.exports = router;
