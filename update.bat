7d21baffe5b83f483c2e6bbfd8e0feb83b8e33c3
RedDragon-11 <46798016+RedDragon-11@users.noreply.github.com>
2024-04-15 11:20:15 -0400

    added requirements

--- update.bat
+++ update.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-call npm install -g npm && call npm install -g node && call npm install -g nodemon
+call npm install -g npm && call npm install -g node && call npm install -g nodemon && call npm install -g bcrypt && call npm install -g mongoose
 @ECHO Update Complete! You may now use the Run.bat
 PAUSE
 
